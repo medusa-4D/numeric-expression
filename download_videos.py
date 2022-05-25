806550e2e576cbd8af43a6843f580ae8b29a6fd3
Lukas Snoek <lukassnoek@gmail.com>
2022-05-25 16:25:21 +0100

    Fix encoding bug/remove req

--- download_videos.py
+++ download_videos.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 from tqdm import tqdm
 
-data = pd.read_csv('data/data.csv')
+data = pd.read_csv('data/data.csv', encoding='latin1')
 for idx, row in tqdm(data.iterrows(), total=data.shape[0]):
     id_ = row['ID']
     r = requests.get(row['URL'])
